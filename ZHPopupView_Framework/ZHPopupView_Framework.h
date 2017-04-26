625b8093648c0fc2332e4a93121cb098e03bb047
James Whelton <james@whelton.io>
2017-04-26 20:00:32 +0400

    Added framework for Cartahge support

---
+++ ZHPopupView_Framework/ZHPopupView_Framework.h
@@ -0,0 +1,19 @@
+//
+//  ZHPopupView_Framework.h
+//  ZHPopupView_Framework
+//
+//  Created by James Whelton on 4/26/17.
+//  Copyright © 2017 heyz3a. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ZHPopupView_Framework.
+FOUNDATION_EXPORT double ZHPopupView_FrameworkVersionNumber;
+
+//! Project version string for ZHPopupView_Framework.
+FOUNDATION_EXPORT const unsigned char ZHPopupView_FrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZHPopupView_Framework/PublicHeader.h>
+
+
